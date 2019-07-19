2809bfc3a1d982b9be2cae2371735aef62505019
Kevin C. Escobedo <escobedo001@gmail.com>
2019-07-19 11:47:58 -0700

    Starting to create a database

--- shiny_db.py
+++ shiny_db.py
@@ -1,5 +1,29 @@
 import sqlite3
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Exception as e:
+        print(e)
+    return None
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Exception as e:
+        print(e)
+
 if __name__ == "__main__":
-    conn = sqlite3.connect("test.db")
-    print("Database opened successfully")
+    database = "shiny_pokemon.db"
+    start = """CREATE TABLE IF NOT EXISTS shiny_hunts (
+        target text PRIMARY KEY, gen integer NOT NULL,
+        encounters integer);"""
+    conn = create_connection(database)
+
+    if conn:
+        create_table(conn, start)
+    else:
+        print("Error")
